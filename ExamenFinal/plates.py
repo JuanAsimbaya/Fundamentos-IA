d010663ef50e48910934af8307622369c798466a
Juan Asimbaya <j.asimbaya@protelcotelsa.com>
2026-09-18 16:09:17 -0500

    paths

--- ExamenFinal/plates.py
+++ ExamenFinal/plates.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 import easyocr
 
 # Cargar modelo YOLO entrenado
-custom_model = YOLO(r'C:\Users\j_asi\Documents\Maestria\Fundamentos IA\ExamenFinal\runs\detect\custom_yolo_model\weights\best.pt')
+custom_model = YOLO('best.pt')
 
 # Inicializar OCR
 reader = easyocr.Reader(['en'], gpu=False)
